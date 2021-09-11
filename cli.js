0b7517e9afa648a43d687f175d47b15ff2197027
Max Harlow <maxharlow@gmail.com>
2021-09-11 19:05:56 +0100

    Remove AWS-specific commands

--- cli.js
+++ cli.js
@@ -52,26 +52,6 @@ async function setup() {
             .positional('destination', { type: 'string', describe: 'Destination directory' })
             .option('m', { alias: 'method', type: 'choices', describe: 'Conversion method to use', choices: ['library', 'shell'], default: 'shell' })
     })
-    instructions.command('create-aws-job', 'Create AWS Textract jobs for every file in an S3 Bucket', args => {
-        args
-            .usage('Usage: ocracy create-aws-job <origin> <jobfile>')
-            .demandCommand(2, '')
-            .positional('origin', { type: 'string', describe: 'Origin S3 Bucket path' })
-            .positional('jobfile', { type: 'string', describe: 'A jobfile' })
-    })
-    instructions.command('get-aws-job-status', 'Get the status for every AWS Textract job in a jobfile', args => {
-        args
-            .usage('Usage: oracy get-aws-job-status <jobfile>')
-            .demandCommand(1, '')
-            .positional('jobfile', { type: 'string', describe: 'A jobfile' })
-    })
-    instructions.command('convert-aws-job-to-text', 'Extract the text from every AWS Textract job in a jobfile', args => {
-        args
-            .usage('Usage: oracy convert-aws-job-to-text <jobfile> <destination>')
-            .demandCommand(1, '')
-            .positional('jobfile', { type: 'string', describe: 'A jobfile' })
-            .positional('destination', { type: 'string', describe: 'A destination directory to write text files' })
-    })
     if (instructions.argv._.length === 0) instructions.showHelp().exit(0)
     const command = instructions.argv._[0]
     try {
@@ -109,32 +89,6 @@ async function setup() {
             const process = await ocracy.extractPDFToText(origin, destination, method, verbose, alert)
             const total = await process.length()
             await process.run().each(ticker('Working...', total))
-        }
-        else if (command === 'create-aws-job') {
-            const {
-                _: [, origin, jobfile],
-                verbose
-            } = instructions.argv
-            console.error('Starting up...')
-            const process = await ocracy.createAWSJob(origin, jobfile, verbose, alert)
-            const total = await process.length()
-            process.run().each(ticker('Working...', total))
-        }
-        else if (command === 'get-aws-job-status') {
-            const {
-                _: [, jobfile]
-            } = instructions.argv
-            const process = await ocracy.getAWSJobStatus(jobfile)
-            process().each(job => alert(`${job.bucket}\t${job.prefix}\t${job.key}\t${job.state}`))
-        }
-        else if (command === 'convert-aws-job-to-text') {
-            const {
-                _: [, jobfile, destination]
-            } = instructions.argv
-            console.error('Starting up...')
-            const process = await ocracy.convertAWSJobToText(jobfile, destination, alert)
-            const total = await process.length()
-            process.run().each(ticker('Working...', total))
         }
         else {
             throw new Error(`${command}: unknown command`)

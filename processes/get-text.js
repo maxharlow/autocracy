0434c473eaffa512f7da3579e127007a56a8f960
Max Harlow <maxharlow@gmail.com>
2021-10-17 17:21:17 +0100

    Split arguments

--- processes/get-text.js
+++ processes/get-text.js
@@ -7,23 +7,55 @@ async function initialise(origin, destination, forceOCR, verbose, alert) {
     const sequence = [
         !forceOCR && {
             name: 'Extracting PDFs to full texts',
-            setup: () => autocracy.operations.extractPDFToText(origin, destination, 'shell', verbose, alert)
+            setup: () => autocracy.operations.extractPDFToText(
+                origin,
+                destination,
+                'shell',
+                verbose,
+                alert
+            )
         },
         !forceOCR && {
             name: 'Symlinking untagged PDFs',
-            setup: () => autocracy.operations.symlinkMissing(origin, destination, cacheUntagged, verbose, alert)
+            setup: () => autocracy.operations.symlinkMissing(
+                origin,
+                destination,
+                cacheUntagged,
+                verbose,
+                alert
+            )
         },
         {
             name: forceOCR ? 'Converting PDFs to JPEG pages' : 'Converting untagged PDFs to JPEG pages',
-            setup: () => autocracy.operations.convertPDFToJpegPages(forceOCR ? origin : cacheUntagged, cacheJpegPages, 'shell', 300, verbose, alert)
+            setup: () => autocracy.operations.convertPDFToJpegPages(
+                forceOCR ? origin : cacheUntagged,
+                cacheJpegPages,
+                'shell',
+                300,
+                verbose,
+                alert
+            )
         },
         {
             name: 'Converting JPEG pages to text pages',
-            setup: () => autocracy.operations.convertJpegPagesToTextPages(cacheJpegPages, cacheTextPages, 'shell', 'eng', 300, verbose, alert)
+            setup: () => autocracy.operations.convertJpegPagesToTextPages(
+                cacheJpegPages,
+                cacheTextPages,
+                'shell',
+                'eng',
+                300,
+                verbose,
+                alert
+            )
         },
         {
             name: 'Combining text pages into full texts',
-            setup: () => autocracy.operations.combineTextPages(cacheTextPages, destination, verbose, alert)
+            setup: () => autocracy.operations.combineTextPages(
+                cacheTextPages,
+                destination,
+                verbose,
+                alert
+            )
         }
     ]
     return sequence.filter(x => x)
--- processes/make-searchable.js
+++ processes/make-searchable.js
@@ -7,23 +7,56 @@ async function initialise(origin, destination, forceOCR, verbose, alert) {
     const sequence = [
         !forceOCR && {
             name: 'Copying already-tagged PDFs',
-            setup: () => autocracy.operations.copyPDFTagged(origin, destination, 'shell', verbose, alert)
+            setup: () => autocracy.operations.copyPDFTagged(
+                origin,
+                destination,
+                'shell',
+                verbose,
+                alert
+            )
         },
         !forceOCR && {
             name: 'Symlinking untagged PDFs',
-            setup: () => autocracy.operations.symlinkMissing(origin, destination, cacheUntagged, verbose, alert)
+            setup: () => autocracy.operations.symlinkMissing(
+                origin,
+                destination,
+                cacheUntagged,
+                verbose,
+                alert
+            )
         },
         {
             name: forceOCR ? 'Converting PDFs to JPEG pages' : 'Converting untagged PDFs to JPEG pages',
-            setup: () => autocracy.operations.convertPDFToJpegPages(forceOCR ? origin : cacheUntagged, cacheJpegPages, 'shell', 300, verbose, alert)
+            setup: () => autocracy.operations.convertPDFToJpegPages(
+                forceOCR ? origin : cacheUntagged,
+                cacheJpegPages,
+                'shell',
+                300,
+                verbose,
+                alert
+            )
         },
         {
             name: 'Converting JPEG pages to PDF pages',
-            setup: () => autocracy.operations.convertJpegPagesToPDFPages(cacheJpegPages, cachePDFPages, 'shell', 'eng', 300, verbose, alert)
+            setup: () => autocracy.operations.convertJpegPagesToPDFPages(
+                cacheJpegPages,
+                cachePDFPages,
+                'shell',
+                'eng',
+                300,
+                verbose,
+                alert
+            )
         },
         {
             name: 'Combining PDF pages',
-            setup: () => autocracy.operations.combinePDFPages(cachePDFPages, destination, 'shell', verbose, alert)
+            setup: () => autocracy.operations.combinePDFPages(
+                cachePDFPages,
+                destination,
+                'shell',
+                verbose,
+                alert
+            )
         }
     ]
     return sequence.filter(x => x)

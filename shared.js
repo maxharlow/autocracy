4f224af48f9e30d5f1ee10197655597acc356f16
Max Harlow <maxharlow@gmail.com>
2023-04-05 18:15:51 +0100

    Replace Klaw due to hitting call stack size limit

--- shared.js
+++ shared.js
@@ -1,15 +1,23 @@
 import Path from 'path'
+import FSExtra from 'fs-extra'
 import Scramjet from 'scramjet'
-import Klaw from 'klaw'
+
+async function* walk(pathRoot) {
+    const filenames = await FSExtra.readdir(pathRoot)
+    for (const filename of filenames) {
+        const path = Path.resolve(pathRoot, filename)
+        if ((await FSExtra.stat(path)).isDirectory()) yield* walk(path)
+        else yield path
+    }
+}
 
 function source(origin, destination, { paged, originInput } = {}) {
     const originResolved = Path.resolve(origin)
-    const listing = Klaw(origin)
+    const listing = walk(origin)
     return Scramjet.DataStream.from(listing).map(entry => {
-        if (!entry.stats.isFile()) return
-        const name = entry.path.replace(`${originResolved}/`, '')
+        const name = entry.replace(`${originResolved}/`, '')
         return {
-            name: paged ? Path.dirname(entry.path).replace(`${originResolved}/`, '') : name, // if origin is page-files, 'name' should refer to the original document name
+            name: paged ? Path.dirname(entry).replace(`${originResolved}/`, '') : name, // if origin is page-files, 'name' should refer to the original document name
             input: `${originInput || origin}/${name}`, // originInput used when the origin we are listing is not the actual location of the files we want to use
             output: `${destination}/${name}`
         }
